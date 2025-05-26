3c40bd1fc8011e89842109dcd7995af415911178
cshegolkov <cshegolkov.us@gmail.com>
2025-05-25 21:14:12 -0700

    www

--- Tests/test_temporary.py
+++ Tests/test_temporary.py
@@ -7,6 +7,7 @@ from conftest import driver
 def test_billy_al(driver):
     page = BasePage(driver, "https://billyal.netlify.app/")
     page.open()
+    driver.save_screenshot('screenshot1.png')
     time.sleep(2)  # чтобы увидеть страницу
 # находим элемент и кликаем по нему
     elements = driver.find_elements(By.CSS_SELECTOR, '.mx-3.my-1.text-dark.text-decoration-none.fw-bold')
@@ -14,5 +15,6 @@ def test_billy_al(driver):
         elements[1].click()  # клик по первому элементу
     else:
         print("Элементы не найдены")
+    driver.save_screenshot('screenshot2.png')
 
     time.sleep(5)
